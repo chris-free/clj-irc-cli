0878b3c5324f5338d27bc68b561394de13aa328f
Chris Freeman <chris.fy@gmail.com>
2014-03-11 18:02:50 +0000

    Moving around! Hopefully it works! :)

--- src/clj_irc_cli/core.clj
+++ src/clj_irc_cli/core.clj
@@ -3,28 +3,27 @@
 ;; http://www.haskell.org/haskellwiki/Roll_your_own_IRC_bot#Where_to_now.3F
 
 (ns clj-irc-cli.core
-  (:require [clojure.core.async :as a]))
+  (:require [clojure.core.async :refer [go-loop]]
+            [aleph.tcp :refer [tcp-client]]
+            [lamina.core :refer [enqueue wait-for-message wait-for-result]]
+            [gloss.core :refer [string]]))
 
-(use 'lamina.core 'aleph.tcp 'gloss.core)
 
-#_(do
-    (def ch
-      (wait-for-result
-       (tcp-client {:host "irc.freenode.net",
-                    :port 6667,
-                    :frame (string :utf-8 :delimiters ["\r\n"])})))
-
-    
-    (a/go-loop []
+#_(let [ch
+        (wait-for-result
+         (tcp-client {:host "irc.freenode.net",
+                      :port 6667,
+                      :frame (string :utf-8 :delimiters ["\r\n"])}))]
+    (go-loop []
       (when-let [msg (wait-for-message ch)]
         (when (.contains msg "PING")
           (enqueue ch "PONG"))
         (println msg)
         (recur)))
-
+  
     (enqueue ch "NICK k3ny")
     (enqueue ch "USER k3ny 8 *  : Paul Mutton")
     (enqueue ch "JOIN #clojure")
     (enqueue ch "PRIVMSG #clojure :test..") ;; Self messages aren't returned from the server
-    )
+    (enqueue ch "PRIVMSG test_ :test.."))
 
